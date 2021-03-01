08a166a83fbdf9595e384ba9db72d700e57570d9
shushoq <wywkoff@gmail.com>
2021-03-01 10:52:57 +0300

    refactor: change resolve for better readability

--- src/what-season.js
+++ src/what-season.js
@@ -5,8 +5,8 @@ module.exports = function getSeason(date ) {
   if(isNaN(date.getTime())) throw Error('Error')
   
   let month = date.getMonth() + 1
-  if(month > 2 && month < 6) return 'spring'
-  if(month > 5 && month < 9) return 'summer'
-  if(month > 8 && month < 12) return 'autumn'
+  if(month >= 3 && month <= 5) return 'spring'
+  if(month >= 6 && month <= 8) return 'summer'
+  if(month >= 9 && month <= 11) return 'autumn'
   else return 'winter'
 };
